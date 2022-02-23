d747cd9c240bdca9e370a48379866a5556f579fe
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-02-22 22:24:51 -0600

    formatted document

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 // parse incoming string or array data
@@ -27,16 +25,16 @@ function filterByQuery(query, animalsArray) {
         }
         // Loop through each trait in the personalityTraits array:
         personalityTraitsArray.forEach(trait => {
-        // Check the trait against each animal in the filteredResults array.
-        // Remember, it is initially a copy of the animalsArray,
-        // but here we're updating it for each trait in the .forEach() loop.
-        // For each trait being targeted by the filter, the filteredResults
-        // array will then contain only the entries that contain the trait,
-        // so at the end we'll have an array of animals that have every one 
-        // of the traits when the .forEach() loop is finished.
-        filteredResults = filteredResults.filter(
-            animal => animal.personalityTraits.indexOf(trait) !== -1
-        );
+            // Check the trait against each animal in the filteredResults array.
+            // Remember, it is initially a copy of the animalsArray,
+            // but here we're updating it for each trait in the .forEach() loop.
+            // For each trait being targeted by the filter, the filteredResults
+            // array will then contain only the entries that contain the trait,
+            // so at the end we'll have an array of animals that have every one 
+            // of the traits when the .forEach() loop is finished.
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
         });
     }
     if (query.diet) {
@@ -64,7 +62,7 @@ function createNewAnimal(body, animalsArray) {
 
     fs.writeFileSync(
         path.join(__dirname, './data/animals.json'),
-        JSON.stringify({ animals: animalsArray}, null, 2)
+        JSON.stringify({ animals: animalsArray }, null, 2)
     );
     // return finished code to post route for response
     return animal;
@@ -112,10 +110,10 @@ app.post('/api/animals', (req, res) => {
     if (!validateAnimal(req.body)) {
         res.status(400).send('The animal is not properly formatted.');
     } else {
-    // add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
-    // req.body is where our incoming content 
-    res.json(animal);
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        // req.body is where our incoming content 
+        res.json(animal);
     }
 });
 
